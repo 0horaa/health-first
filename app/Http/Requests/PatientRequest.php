3df6a5638832514b3699acacb1e3644b82fa9a9a
RomuloLim <kinggbr0102@gmail.com>
2022-10-01 17:18:50 -0300

    ready

---
+++ app/Http/Requests/PatientRequest.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class PatientRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|min:6|max:80',
+            'avatar' => 'mimes:jpg,jpeg,png',
+            'age' => 'required|integer|min:1|max:120',
+            'cpf' => 'required|cpf',
+            'observation' => 'nullable',
+            'social_name' => 'nullable',
+            'symptoms' => 'nullable'
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            '*.required' => 'Não foi possível fazer o registro do paciente. Por favor, preencha todos os campos obrigatórios.',
+            '*.min' => 'Não foi possível fazer o registro do paciente. Por favor, preencha todos os campos com os caracteres mínimos.',
+            '*.max' => 'Não foi possível fazer o registro do paciente. Por favor, preencha todos os campos com os caracteres máximos.',
+            '*.mimes' => 'Não foi possível fazer o registro do paciente. Por favor, selecione uma imagem válida para definir o avatar.',
+            '*.unique' => 'Não foi possível fazer o registro do paciente. Este CPF já está em uso.',
+            'cpf.cpf' => 'Não foi possível fazer o registro do paciente. Por favor, insira um CPF válido.',
+        ];
+    }
+}
--- app/Models/Patient.php
+++ app/Models/Patient.php
@@ -9,9 +9,69 @@ class Patient extends Model
 {
     use HasFactory;
 
+    protected $appends = [
+        'status', //define uma forma de chamar o getStatusAttribute usando apenas uma propriedade chamada 'status'
+        'color'
+    ];
+
     protected $casts = [
         'symptoms' => 'array'
     ];
 
-    protected $guarded = [];
+    protected $guarded = [
+        'id',
+        'created_at',
+    ];
+
+    protected $fillable = [
+        'name',
+        'avatar',
+        'age',
+        'phone',
+        'cpf',
+        'observation',
+        'social_name',
+        'symptoms'
+    ];
+
+    protected function verifySymptoms($result1, $result2, $result3, $result4) {
+        $symptoms = $this->attributes['symptoms'];
+
+        if(isset($symptoms) && count(json_decode($symptoms)) <= 5) {
+            return $result1;
+        } else if(isset($symptoms) && count(json_decode($symptoms)) <= 8) {
+            return $result2;
+        } else if(isset($symptoms) && count(json_decode($symptoms)) > 8) {
+            return $result3;
+        } else {
+            return $result4;
+        }
+    }
+
+    public function getNameAttribute() {
+        if(isset($this->attributes['social_name'])) {
+            return $this->attributes['social_name'];
+        } else {
+            return $this->attributes['name'];
+        }
+    }
+
+    public function getStatusAttribute() {
+        return $this->verifySymptoms(
+            'Sintomas insuficientes',
+            'Potencial infectado',
+            'Possível infectado',
+            'Nenhum sintoma foi informado'
+        );
+    }
+
+    public function getColorAttribute() {
+        return $this->verifySymptoms(
+            'card mb-3 text-white bg-success',
+            'card mb-3 text-white bg-warning',
+            'card mb-3 text-white bg-danger',
+            'card mb-3 text-white bg-success'
+        );
+    }
+
 }
